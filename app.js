98f0baf63e813fc861291e9bce141de6a8401c14
Sakina Shivji <shivjisakina@gmail.com>
2017-08-10 21:38:40 -0400

    finished start game function

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var languageArray = ["Javascript", "PHP", "Python", "Pascal", "Cobol", "Ruby", "C", "SQL"];
 
 // Empty string for the computer selected word
-var computerChoice = "";
+var chosenWord = "";
 
 // Breaking the word up by letters in an array
 var lettersArray = [];
@@ -15,6 +15,9 @@ var letterGuessed = "";
 
 // The number of blanks we show based on the solution.
 var numBlanks = 0;
+
+// Holds all of the wrong guesses.
+var wrongGuesses = [];
 
 // Holds a mix of blank and solved letters (ex: 'n, _ _, n, _').
 var blanksAndSuccesses = [];
@@ -36,4 +39,35 @@ var startGame = function () {
     chosenWord = languageArray[Math.floor(Math.random() * languageArray.length)];
 
     // Splitting the word and putting it in the lettersArray
+    lettersArray = chosenWord.split("");
+
+    // Counting the number of blanks depending on the word chosen
+    numBlanks = lettersArray.length;
+
+    // Reset the guess and success array after each round
+    blanksAndSuccesses = [];
+
+    // Reset the wrong guesses after each round.
+    wrongGuesses = [];
+
+    // Fill up the blanksAndSuccesses list with number of blanks according to the word.
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSuccesses.push("_");
+    }
+
+    // Print the initial blanks in console.
+    console.log(blanksAndSuccesses);
+
+    // Reprints the guessesLeft to 9.
+    document.getElementById("guessesLeft").innerHTML = Guesses;
+
+    // Prints the blanks at the beginning of each round in the HTML.
+    document.getElementById("wordBlanks").innerHTML = blanksAndSuccesses.join(" ");
+
+    // Clears the wrong guesses from the previous round.
+    document.getElementById("wrongGuesses").innerHTML = wrongGuesses.join(" ");
+
+
 }
+
+startGame()
