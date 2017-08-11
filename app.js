02b0a0647a7da0802ca276ad157dac3dceeb331a
Sakina Shivji <shivjisakina@gmail.com>
2017-08-10 21:27:06 -0400

    got global variables

---
+++ app.js
@@ -0,0 +1,12 @@
+// GLOBAL VARIABLES
+// ==================================================================================================
+
+// Creating an array of languages
+var languageArray = ["Javascript", "PHP", "Python", "Pascal", "Cobol", "Ruby", "C", "SQL"];
+
+// Empty string for the computer selected word
+var computerChoice = "";
+
+// Breaking the word up by letters in an array
+var lettersArray = [];
+
